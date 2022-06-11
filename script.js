79c6b772826ba61fcda6942b5c5e8cb88f8965a7
racynibaya <106055196+racynibaya@users.noreply.github.com>
2022-06-10 21:17:40 -0400

    added event click to h3

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log(document.querySelector('h1').textContent);
+
+const h3 = document.querySelector('h3');
+
+h3.addEventListener('click', () => {
+  console.log(h3.classList.toggle('blue'));
+});
